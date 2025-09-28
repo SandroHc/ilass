af605f67d336197e2024309a3c835c78e9f834d0
Sandro Marques <me@sandro.dev>
2025-09-28 12:26:24 +0900

    nix: Include FFmpeg libs

--- flake.nix
+++ flake.nix
@@ -3,55 +3,75 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs: let
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    forEachSupportedSystem = f:
-      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
-          pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              inputs.rust-overlay.overlays.default
-              inputs.self.overlays.default
-            ];
-          };
-        });
-  in {
-    overlays.default = final: prev: {
-      rustToolchain = prev.rust-bin.stable.latest.default.override {
-        extensions = ["rust-src" "rustfmt"];
-      };
-    };
-
-    devShells = forEachSupportedSystem ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          rustToolchain
-          pkg-config
-          cargo-edit
-          cargo-insta
-          cargo-watch
-          rust-analyzer
-
-          # project specific
-          ffmpeg
-
-          # Windows packaging
-          zip
-          unzip
-        ];
-
-        env = {
-          # Required by rust-analyzer
-          RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {allowUnfree = true;};
+          overlays = [rust-overlay.overlays.default];
         };
-      };
-    });
-  };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = ["rust-src" "rustfmt" "clippy"];
+        };
+
+        ffmpeg-full = pkgs.ffmpeg-full.override {
+          withUnfree = true;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustToolchain
+            cargo-edit
+            cargo-insta
+            cargo-watch
+
+            clang
+            nasm
+            pkg-config
+
+            # project specific
+            ffmpeg-full.dev
+
+            # Windows packaging
+            zip
+            unzip
+          ];
+
+          buildInputs = with pkgs; [
+          ];
+
+          shellHook = ''
+            export LIBCLANG_PATH="${pkgs.libclang.lib}/lib"
+
+            echo ""
+            echo "ilass environment:"
+            echo "  Platform: ${system}"
+            echo "  $(rustc --version)"
+            echo "  ffmpeg $(${ffmpeg-full}/bin/ffmpeg -version | head -n1 | awk -F ' ' '{print $3}')"
+            echo ""
+            echo "Available commands:"
+            echo "  cargo build"
+            echo "  cargo build --features static,ffmpeg-next/build"
+            echo "  pkg-config --libs libavcodec"
+            echo ""
+          '';
+
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+        };
+      }
+    );
 }
